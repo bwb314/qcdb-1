66b135da3a9fcca2ee1c41e23f12f25dc991d591
Brandon Bakr <brandon.bakr@gatech.edu>
2017-10-31 16:54:49 -0400

    Made crystal class work, with some basic functionality

--- qcdb/crystal.py
+++ qcdb/crystal.py
@@ -12,18 +12,30 @@ Inputs can be:
 Expects Angstrom input
 """
 import numpy as np
+#generic functions defined outside of class
+cov_rad = {   'H' : 0.37, 'C' : 0.77, 'O' : 0.73, 'N' : 0.75, 'F' : 0.71,
+'P' : 1.10, 'S' : 1.03, 'Cl': 0.99, 'Br': 1.14, 'I' : 1.33, 'He': 0.30,
+'Ne': 0.84, 'Ar': 1.00, 'Li': 1.02, 'Be': 0.27, 'B' : 0.88, 'Na': 1.02,
+'Mg': 0.72, 'Al': 1.30, 'Si': 1.18, 'K' : 1.38, 'Ca': 1.00, 'Sc': 0.75,
+'Ti': 0.86, 'V' : 0.79, 'Cr': 0.73, 'Mn': 0.67, 'Fe': 0.61, 'Co': 0.64,
+'Ni': 0.55, 'Cu': 0.46, 'Zn': 0.60, 'Ga': 1.22, 'Ge': 1.22, 'As': 1.22,
+'Se': 1.17, 'Br': 1.14, 'Kr': 1.03}
+
+
+def dist(a,b):
+    x = (a[0]-b[0])**2
+    y = (a[1]-b[1])**2
+    z = (a[2]-b[2])**2
+    dist = (x+y+z)**0.5
+    return dist
+
+def bound(a_el,a,b_el,b):
+    if cov_rad[a_el]+cov_rad[b_el] >=  dist(a,b): return True
+    else: return False
 
 class crystal():
 
-    cov_rad = {   'H' : 0.37, 'C' : 0.77, 'O' : 0.73, 'N' : 0.75, 'F' : 0.71,
-    'P' : 1.10, 'S' : 1.03, 'Cl': 0.99, 'Br': 1.14, 'I' : 1.33, 'He': 0.30,
-    'Ne': 0.84, 'Ar': 1.00, 'Li': 1.02, 'Be': 0.27, 'B' : 0.88, 'Na': 1.02,
-    'Mg': 0.72, 'Al': 1.30, 'Si': 1.18, 'K' : 1.38, 'Ca': 1.00, 'Sc': 0.75,
-    'Ti': 0.86, 'V' : 0.79, 'Cr': 0.73, 'Mn': 0.67, 'Fe': 0.61, 'Co': 0.64,
-    'Ni': 0.55, 'Cu': 0.46, 'Zn': 0.60, 'Ga': 1.22, 'Ge': 1.22, 'As': 1.22,
-    'Se': 1.17, 'Br': 1.14, 'Kr': 1.03}
-
-    def __init__(self, mol): 
+    def __init__(self, mol, fragment = True): 
         if type(mol) is str and mol.endswith('.xyz'):
             self.fil = mol
             with open(mol,'r') as ofil:
@@ -39,57 +51,68 @@ class crystal():
                     geom = np.vstack([geom,coords])
                 mol = [els,geom]
         self.mol = mol
-        self.frags = []
-
+        if not fragment: self.frags = []
+        else: self.bfs()
     def print_out(self):
-        print("In Angstrom\n")
         els = self.mol[0]
+        print(len(els))
+        print("In Angstrom")
         coords = self.mol[1]
         for i in range(len(els)): 
             el = els[i]
             c = coords[i]
             print("%2s %7.3f %7.3f %7.3f" % (el,c[0],c[1],c[2]))
+        print("")
 
-    
-        
-    def dist(a,b):
-        x = (a[0]-b[0])**2
-        y = (a[1]-b[1])**2
-        z = (a[2]-b[2])**2
-        dist = (x+y+z)**0.5
-        return dist
+    def write_out(self,fil_name):
+        with open(fil_name,'w') as fil:
+            els = self.mol[0]
+            fil.write("%d\n" % (len(els)))
+            fil.write("In Angstrom\n")
+            coords = self.mol[1]
+            for i in range(len(els)): 
+                el = els[i]
+                c = coords[i]
+                fil.write("%2s %7.3f %7.3f %7.3f\n" % (el,c[0],c[1],c[2]))
 
-    def bound(a_el,a,b_el,b):
-        if cov_rad[a_el]+cov_rad[b_el] >=  dist(a,b): return True
-        else: return False
-
-    def intersect(frag,frags):
-        intersects = set() 
-        for f in frags:
-            if frag & f == set([]): continue
-            intersects = intersects | frag | f
-            frags.remove(f) 
-        return intersects
-    
     def bfs(self):
-        minifrags = []
-        geom = self.mol
-        #make sets of bonded atoms
-        for i in range(len(geom)):
+        els = self.mol[0]
+        coords = self.mol[1]
+        lc = len(coords)
+        bonds = []
+        #find all bonded atoms
+        for i in range(1,lc):
             for j in range(i):
-                if bound(geom[i],geom[j]): minifrags.append(set([str(geom[i]),str(geom[j])]))
-        frags = []
-        while minifrags != []:
-            one = intersect(minifrags[0],minifrags)
-            last = []
-            while one != set([]):
-                one = intersect(one,minifrags)
-                if one != set([]): last = one
-            if last != []: frags.append(last)
-        
-        return frags
+                if not bound(els[i],coords[i],els[j],coords[j]): continue
+                bonds.append(set([i,j]))
+        #Construct subsets such that each pair has a null intersection (BFS)
+        minds = []
+        while bonds != []:
+            mol = bonds[0]
+            bonds.remove(bonds[0])
+            intersect = True
+            while intersect: 
+                intersect = False
+                remove = []
+                for i in bonds:
+                    if i & mol == set([]): continue
+                    for j in i: mol.add(j)
+                    intersect = True
+                    remove.append(i)
+                for i in remove: bonds.remove(i)
+            minds.append(mol)
+        #build mol out of separated inds
+        mols = []
+        for mol in minds:
+            mels = []
+            mcoords = []
+            for i in mol:
+                mels.append(els[i])
+                mcoords.append(coords[i])
+            mols.append(crystal([mels,mcoords],fragment = False))
+        self.frags = mols
 
+    
  
 a = crystal("sulfanilamide.xyz")
-print(a.frags)
-print(a.bfs())
+a.frags[0].write_out("TEST.xyz")
